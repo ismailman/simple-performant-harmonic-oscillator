0b5ccd306a5a8e14eafdcedc91071a791fd7c48e
ismailman <code@omar.dev>
2020-06-21 00:01:22 -0400

    remove eval

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './spho.js',
     mode: 'development',
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'spho.js',

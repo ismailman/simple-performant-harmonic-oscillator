87f7e0c180bc90533b178925b917a4b7c1f9da42
ismailman <code@omar.dev>
2020-06-21 00:08:46 -0400

    prod build

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './spho.js',
-    mode: 'development',
-    devtool: 'source-map',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'spho.js',
